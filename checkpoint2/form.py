14fc713f03533264d478b8af165e3aad786c2b08
Mwaleh muturi <alex.mwaleh@andela.com>
2015-11-19 19:15:14 +0300

    Add sign up

--- checkpoint2/form.py
+++ checkpoint2/form.py
@@ -1,9 +1,9 @@
 from flask_wtf import Form 
 from wtforms import StringField, PasswordField, SubmitField 
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class SignupForm(Form):
-	first_name = StringField('first Name', validators=[DataRequired()])
-	last_name = StringField('last Name', validators=[DataRequired()])
-	email = StringField('Email', validators=[DataRequired()])
+	first_name = StringField('first Name', validators=[DataRequired("add first name")])
+	last_name = StringField('last Name', validators=[DataRequired("add Last Name")])
+	email = StringField('Email', validators=[DataRequired("Input email"),Email])
 	submit = SubmitField('Signup')
