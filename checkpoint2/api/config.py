0a884cc44ce4ed2c741dfbb5d147783ec7eed224
Mwaleh muturi <alex.mwaleh@andela.com>
2015-12-18 12:35:29 +0300

    [Chore] Add Tests

--- checkpoint2/api/config.py
+++ checkpoint2/api/config.py
@@ -1,2 +1,3 @@
 POSTS_PER_PAGE = 2
 MAX_PAGES = 100
+TEST_DB = 'postgresql://localhost/Bucket_test'
--- checkpoint2/tests/test_bucketlist.py
+++ checkpoint2/tests/test_bucketlist.py
@@ -1,48 +1,51 @@
-from flask import Flask
+from flask import Flask, g
+from flask.ext.testing import TestCase
 from flask.ext.sqlalchemy import SQLAlchemy
 import sys
-from faker import Factory
+
 sys.path.append('.')
-from api import bucketlist 
+from faker import Factory
+from api.config import TEST_DB
 from api.bucketlist import app
-from api.models import db
+from api.models import db, users
 import unittest
 import json
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 
 fake = Factory.create()
 class BucketlistTestCase(unittest.TestCase):
-    SQLALCHEMY_DATABASE_URI = "postgres://"
-    TESTING = True
-    db = SQLAlchemy()
+
 
     username = fake.name()
     password = fake.password()
+   
     
-        
-       
+
+    # Create the app
     def create_app(self):
-        app = Flask(__name__)
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'
-        app.config['SECRET_KEY'] = "development-Key"
-        #app.config["JSON_SORT_KEYS"] = False
-        
-        
-        return app
-
-    def setUp(self):
-       
-        bucketlist.app.config['TESTING']= True
-        self.app = bucketlist.app.test_client()
-        self.client = bucketlist.app.test_client()
-
+        return create_app(self)
+    # set up the app config database and 
+    @classmethod
+    def  setUpClass(self):
         with app.test_request_context():
-                # Extensions like Flask-SQLAlchemy now know what the "current" app
-            db.init_app(app)    # is while within this block. Therefore, you can now run........
-            
-        with app.test_request_context():
+            app.config['SECRET_KEY'] = "development-test-Key"
+            app.config['TESTING'] = True
+            # change config to use test database
+            app.config['SQLALCHEMY_DATABASE_URI']= TEST_DB
+            self.app = app.test_client()
+            self.token = ''
+            db.init_app(app) 
+            #db.session.close() 
             db.create_all()
-    
-        
+     # Clean up        
+    @classmethod
+    def tearDownClass(self):
+        with app.test_request_context():
+            db.session.close()
+            db.drop_all()
+            db.session.remove()
+       
     def test_home_page(self):
         res =  self.app.get('/')
         assert 'info' in res.data
@@ -60,13 +63,78 @@ class BucketlistTestCase(unittest.TestCase):
         
         data=json.dumps({"username": self.username, "password": self.password})
         res = self.app.post('/api/users', data=data, content_type='application/json' )
+        
         assert res.status_code == 201
+        
         # check if user has been added
 
+    def test_login(self) :
+        data=json.dumps({"username": self.username, "password": self.password})
+        res = self.app.post('/auth/login', data=data, content_type='application/json')
+        assert res.status_code == 200
+        assert 'token' in res.data
+        # if token is returned then user has been looged in 
+        # decode json object
+        json_data = json.loads(res.data)
+        app.token = json_data['token']
+        # try acessing a page once logged in 
+        data=json.dumps({"username": self.username, "password": self.password})
+        # Try accessing an authenicated page 
+        res = self.app.get('/bucketlists')
+        assert res.status_code == 401
 
-    # def tearDown(self):
-    #     db.session.remove()
-    #     db.drop_all
+    def test_token_authenication (self):
+        # Access Page with Token
+        header ={
+                    'Content-Type':'application/json' ,
+                    'token': app.token
+                }
+        # Try accessing page with Token
+        res = self.app.get('/bucketlists',headers=header)
+        assert res.status_code == 200
+        assert 'Bucketlist' in res.data
+        
+
+    def test_bucketlist_addition(self):
+        data ={
+                'name' : "Test_list"
+
+              }
+        pass
+    def test_bucketlist_update(self):
+        pass
+    def test_bucketlist_delete(self):
+        pass
+
+    def test_item_addition(self):
+        pass
+    def test_item_update(self):
+        pass
+    def test_item_deletion(self):
+        pass
+
+    def test_pages(self):
+        pass
+
+    def test_page_range(self):
+        pass
+
+    def test_page_limit(self):
+        pass
+    def test_bucketlist_search(self):
+        pass
+    
+
+
+
+
+
+
+
+
+
+
+    
 
 
     
